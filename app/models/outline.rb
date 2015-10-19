51709171aa3b5bc0a24b74affe7ce4bb51aa2d7c
Alex Clarke <aclar48@uwo.ca>
2015-10-19 14:41:59 -0400

    added basic model relations and tests

--- app/models/outline.rb
+++ app/models/outline.rb
@@ -1,2 +1,5 @@
 class Outline < ActiveRecord::Base
+  belongs_to :user
+  has_many :outline_elements
+  has_many :elements, through: :outline_elements
 end
--- app/models/outline_element.rb
+++ app/models/outline_element.rb
@@ -1,2 +1,4 @@
 class OutlineElement < ActiveRecord::Base
+  belongs_to :outline
+  belongs_to :element
 end
