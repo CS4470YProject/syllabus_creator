5b4e42019a7b133b3ecd4c030de25c9610d7829e
Alex Clarke <aclar48@uwo.ca>
2015-10-23 20:43:02 -0400

    debug

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,13 +34,7 @@ set :tests, []
 # which config files should be copied by deploy:setup_config
 # see documentation in lib/capistrano/tasks/setup_config.cap
 # for details of operations
-set(:config_files, %w(
-  nginx.conf
-  log_rotation
-  monit
-  unicorn.rb
-  unicorn_init.sh
-))
+set(:config_files, %w(nginx.conf log_rotation monit unicorn.rb unicorn_init.sh))
 
 # which config files should be made executable after copying
 # by deploy:setup_config
