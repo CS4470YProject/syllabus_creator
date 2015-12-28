4b072651f4458dadf852f58ccd76bc16acd3d230
Alex Clarke <aclar48@uwo.ca>
2015-12-28 15:40:16 -0500

    fixed tests

--- spec/factories/outlines.rb
+++ spec/factories/outlines.rb
@@ -1,10 +1,11 @@
 FactoryGirl.define do
   factory :outline do
-    course FactoryGirl.create(:course, code: 'cs4499')
+    course
   end
 
   factory :template, parent: :outline, class: 'Template' do
     department
+    course nil
   end
 
 end
