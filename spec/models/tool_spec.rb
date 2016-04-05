2ef849985a77d50b52c0f23389db3b7dadad6b16
tlaporte <travislaporte@gmail.com>
2016-04-04 21:07:58 -0400

    Replaced really dumb tests with slightly less dumb tests

--- spec/models/tool_spec.rb
+++ spec/models/tool_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Tool, type: :model do
       @faculty3 = FactoryGirl.create(:faculty, name: 'biology')
       @tool = FactoryGirl.create(:tool, faculty: @faculty)
       @tool2 = FactoryGirl.create(:tool, faculty: @faculty)
-      @tool3 = FactoryGirl.create(:tool, faculty: @faculty2)
+      @tool3 = FactoryGirl.create(:tool, faculty: @faculty2, element: @element)
     end
     describe 'list query' do
       it 'should return the tools for the given faculty' do
@@ -28,18 +28,16 @@ RSpec.describe Tool, type: :model do
     end
     describe 'insert' do
       it 'should add a new tool to the database' do
-        newTool = Tool.insert('This is a newly added tool!', @element.id, @faculty3.id)
-        queryTool = Tool.list_query(@faculty3.id)
-        expect(queryTool[0].name).to eq('This is a newly added tool!')
-        expect(queryTool[0].element_id).to eq(@element.id)
-        expect(queryTool[0].faculty_id).to eq(@faculty3.id)
+        newTool = Tool.insert('insertTest', @element.id, @faculty3.id)
+        expect(Tool.where(name: "insertTest", element_id: @element.id,
+                          faculty_id: @faculty3.id)).to exist
       end
     end
     describe 'remove' do
       it 'should remove the specified tool from the database' do
-        addedTool = Tool.list_query(@faculty2.id)
-        Tool.remove(addedTool[0].id)
-        expect(Tool.list_query(@faculty2.id).size).to eq(0)
+        Tool.remove(@tool3.id)
+        expect(Tool.where(name: "Test Tool", element_id: @element.id,
+                          faculty_id: @faculty2.id)).not_to exist
       end
     end
   end
